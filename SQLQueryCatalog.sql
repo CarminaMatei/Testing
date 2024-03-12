980c7acbaf89142d92222069c93e35be1622e4ad
Carmina-Andra Matei <carmina.matei.99@gmail.com>
2024-03-12 19:45:26 +0200

    Create SQLQueryCatalog.sql
    First commit

---
+++ SQLQueryCatalog.sql
@@ -0,0 +1,11 @@
+SELECT * FROM Elevi;
+
+SELECT Nume, Prenume FROM Elevi;
+
+SELECT ID_elev, ID_profesor, Nota FROM Note WHERE Nota < 5;
+
+SELECT ID_materie , Nota FROM Note WHERE Nota > 7;
+
+SELECT Prenume, Telefon FROM Profesori;
+
+SELECT  * FROM Note WHERE Data = '2024-02-12';
